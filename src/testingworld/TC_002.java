0a5d029c625fa81e1aa241ee78e1e05bb389f2cf
charu2372 <charu.patel2015@gmail.com>
2019-06-13 11:10:41 -0700

    committed file

---
+++ src/testingworld/TC_002.java
@@ -0,0 +1,21 @@
+package testingworld;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_002 {
+	@Test
+	public void testcase() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\adin\\eclipse-workspace\\Day1S\\drivers\\chromedriver.exe");
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("charumati");
+		driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("welcome");
+		//driver.findElement(By.id("email")).sendKeys("charu.patel2372@gmail.com");
+		//driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("pinkey9004");
+		//driver.findElement(By.xpath("//*[@id=\"u_0_2\"]")).click();
+		driver.quit();
+	}
+
+}
